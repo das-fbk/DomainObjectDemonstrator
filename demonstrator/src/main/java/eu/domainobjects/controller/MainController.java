6cde361d6b0c725dff45d0c26fc5e9d7a9da17c5
antbucc <bucchiarone@fbk.eu>
2017-07-26 11:40:20 +0200

    - new handlers added

--- demonstrator/src/main/java/eu/domainobjects/controller/MainController.java
+++ demonstrator/src/main/java/eu/domainobjects/controller/MainController.java
@@ -46,6 +46,7 @@ import eu.fbk.das.domainobject.executable.DVMDefineDataPatternExecutable;
 import eu.fbk.das.domainobject.executable.InsertDestinationExecutable;
 import eu.fbk.das.domainobject.executable.InsertOptionalDataExecutable;
 import eu.fbk.das.domainobject.executable.Rome2RioCallExecutable;
+import eu.fbk.das.domainobject.executable.STMServiceCallExecutable;
 import eu.fbk.das.domainobject.executable.SelectPlanningModeExecutable;
 import eu.fbk.das.domainobject.executable.ShowResultsExecutable;
 import eu.fbk.das.domainobject.executable.StartChatbotExecutable;
@@ -218,12 +219,12 @@ public class MainController {
 			String botName = nameValues[1];
 			String botToken = tokenValues[1];
 
-			bot = new TravelAssistantBot("TestTravelAssistantBot",
-					"348692232:AAGyApErXx36PFRisENTClY1jEsYgZcvbTI", false,
-					false, false, false, aListner, event);
+			// bot = new TravelAssistantBot("TestTravelAssistantBot",
+			// "348692232:AAGyApErXx36PFRisENTClY1jEsYgZcvbTI", false,
+			// false, false, false, aListner, event);
 
-			// bot = new TravelAssistantBot(botName, botToken, false, false,
-			// false, false, aListner, event);
+			bot = new TravelAssistantBot(botName, botToken, false, false,
+					false, false, aListner, event);
 
 			BotSession session = api.registerBot(bot);
 		} catch (TelegramApiException e) {
@@ -336,6 +337,10 @@ public class MainController {
 		processEngineFacade.addExecutableHandler(
 				"VT_ServiceCall",
 				new VTServiceCallExecutable(processEngineFacade
+						.getProcessEngine(), bot));
+		processEngineFacade.addExecutableHandler(
+				"STM_ServiceCall",
+				new STMServiceCallExecutable(processEngineFacade
 						.getProcessEngine(), bot));
 
 		// handler for hoaa for pre-phase
